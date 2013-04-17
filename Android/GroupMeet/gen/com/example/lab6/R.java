26dd061d4f34d7c2783ae5c93cd5b5fa28f092d9
Inhwan Lee <ihlee01@hotmail.com>
2013-04-17 16:21:30 -0400

    Login page up to date

--- Android/GroupMeet/gen/com/example/lab6/R.java
+++ Android/GroupMeet/gen/com/example/lab6/R.java
@@ -22,7 +22,6 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int repeat_bg=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
@@ -34,8 +33,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int footer_repeat=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
